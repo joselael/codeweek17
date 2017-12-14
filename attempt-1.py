5bc5e5061acbc9bef2db137fb7d7c79f5db94af6
Jose L. Louis <jllouis@live.com>
2017-12-13 21:36:39 -0500

    added some trivial metrics.

--- attempt-1.py
+++ attempt-1.py
@@ -1,11 +1,12 @@
 # easy attempt: tree SKLearn tree classifier
 
 import csv
+import time
 
 from sklearn import tree
 
 print("getting data....")
-with open('data0.csv', 'rt') as fi:
+with open('creditcardnotime.csv', 'rt') as fi:
     reader = csv.reader(fi)
     data = list(reader)
     header = data[0][0:29]
@@ -13,18 +14,22 @@ with open('data0.csv', 'rt') as fi:
     features = list()
 
     print("formatting and converting data...")
-    # get labels
+    number = 150000
+    # get features and labels
     iterdata = iter(data);
     next(iterdata)  # skip the headers
     for index, value in enumerate(iterdata):
+        if index > number:
+            break
         labels.append(bool(int(value[29])))  # convert 0 to false and 1 to true
         features.append([float(i) for i in value[0:29]])  # convert the first 28 elements of the line to a float list
 
 print("Training decision tree... ")
+begin_time = time.time();
 clf = tree.DecisionTreeClassifier()
 clf.fit(features, labels)
-print("Decision tree training done!")
-
+print("Decision tree training done! Traing duration: ", time.time() - begin_time, " seconds.")
+print("There were ", labels.count(True), " fraud occurrences in ", number, " training samples.")
 # predict outcomes given inputs
 while True:
     x = input("Enter test datum (q to quit): ")
