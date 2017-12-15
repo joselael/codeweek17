c572b72764827082010b6c9faec7456734bebfc5
heeseungp <heeseung5@gmail.com>
2017-12-15 01:57:15 -0500

    Add bootstrap template, ready to start

--- flaskapp/app/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, url_for, redirect
-from app import app
-
-@app.route('/')
-def index():
-	#homepage
-	return render_template('index.html')
--- flaskapp/run.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-PORT = 5000
-
-if __name__ == "__main__":
-    app.run(port=PORT, debug=True)
