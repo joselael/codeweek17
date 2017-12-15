9f33bb8a51210edc78b5822cd5b8210c445d3124
heeseungp <heeseung5@gmail.com>
2017-12-15 05:14:40 -0500

    Complete front end development, now need to complete the integration with scikit learn

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,7 +1,49 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static/data'
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('result',
+                                    filename=filename))
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['GET','POST'])
+def result():
+	if request.method == 'POST':
+		return redirect(url_for('index'))
+	elif request.method == 'GET':
+		return render_template('result.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
